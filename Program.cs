06b5bdb269d6b7f198437ec7c8d1e0ca4a638ca3
vladpotuk <vladpotuk01@gmail.com>
2024-02-23 19:52:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть речення:");
+        string sentence = Console.ReadLine();
+
+        int wordCount = CountWords(sentence);
+
+        Console.WriteLine($"Кількість слів у реченні: {wordCount}");
+
+        Console.ReadLine();
+    }
+
+    static int CountWords(string sentence)
+    {
+        
+        string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+      
+        return words.Length;
+    }
+}
+
